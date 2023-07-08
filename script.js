54bdbe7b93c9413d5436e6a3cdac5d4f2746e52c
bernyig <bernardommguerreiro@gmail.com>
2023-07-08 15:50:25 +0200

    basic math functions

---
+++ script.js
@@ -0,0 +1,28 @@
+function addNumbers (num1, num2){
+  return num1 + num2;
+}
+
+function subNumbers (num1, num2){
+  return num1 - num2;
+}
+
+function multiplyNumbers(num1, num2){
+  return num1 * num2;
+}
+
+function divideNumbers(num1, num2){
+  return num1 / num2;
+}
+ 
+
+let result1 = addNumbers(4, 4)
+console.log(result1);
+
+let result2 = subNumbers(4, 4)
+console.log(result2);
+
+let result3 = multiplyNumbers(4, 4)
+console.log(result3);
+
+let result4 = divideNumbers(4, 4)
+console.log(result4);
