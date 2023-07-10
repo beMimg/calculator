7b50b4bc6f02621342a64cfcd5e00b58e54a258d
bernyig <bernardommguerreiro@gmail.com>
2023-07-10 13:19:20 +0200

    equal, clear functionality

--- script.js
+++ script.js
@@ -3,47 +3,14 @@ const currentScreenNumber = document.querySelector(".current_number");
 const numberButton = document.querySelectorAll(".number");
 const operatorButton = document.querySelectorAll(".operator");
 const clearButton = document.querySelector("#clear");
+clearButton.addEventListener('click', clearOperator);
 const decimalButton = document.querySelector('.decimal');
 const equalButton = document.querySelector(".equal");
+equalButton.addEventListener('click', operate);
 
 let currentNum = "";
 let operator = "+";
 let previousNum = "";
-
-function addNumbers (num1, num2){
-  return num1 + num2;
-}
-
-function subNumbers (num1, num2){
-  return num1 - num2;
-}
-
-function multiplyNumbers(num1, num2){
-  return num1 * num2;
-}
-
-function divideNumbers(num1, num2){
-  return num1 / num2;
-}
-
-function operate(operator, num1, num2) {
-  if (operator === "+"){
-    return addNumbers(num1, num2);
-  
-  } else if (operator === "-"){
-    return subNumbers(num1, num2);
-  
-  } else if (operator === "*"){
-    return multiplyNumbers(num1, num2); 
-  
-  } else if (operator === "/"){
-    return divideNumbers(num1, num2);
-  
-  } else {
-    console.log("invalid operator");
-    return null;
-  }
-}
 
 numberButton.forEach(btn => {
   btn.addEventListener("click", () => handleNumber(btn.textContent));
@@ -66,4 +33,48 @@ function handleOperator(op){
   previousScreenNumber.textContent = previousNum + operator;
   currentNum = "";
   currentScreenNumber = "";
+}
+
+function operate(){
+  previousNum = Number(previousNum);
+  currentNum = Number(currentNum);
+
+  if (operator === "+"){
+    previousNum = previousNum + currentNum;
+  } else if (operator === "-"){
+    previousNum = previousNum - currentNum;
+  } else if (operator === "*"){
+    previousNum = previousNum * currentNum;
+  } else if (operator === "/"){
+    if (currentNum < 0){
+      currentNum = "error";
+      screenInput();
+      return;
+    }
+    previousNum /= currentNum;
+  }
+  previousNum = previousNum.toString();
+  screenInput();
+}
+
+function roundDecimals (num){
+  return Math.round(num * 100000) / 100000;
+}
+
+function screenInput(){
+  previousScreenNumber.textContent = "";
+  operator = "";
+  if(previousNum.length <= 10){
+    currentScreenNumber.textContent = previousNum;
+  } else {
+    currentScreenNumber.textContent = previousNum.slice(0,10) + "...";
+  }
+}
+
+function clearOperator(){
+  currentNum = "";
+  previousNum = "";
+  operator = "";
+  currentScreenNumber.textContent = "0";
+  previousScreenNumber.textContent = "";
 }
