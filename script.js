e3945f695e2e754824e4a7117d650b13e046c800
bernyig <bernardommguerreiro@gmail.com>
2023-07-10 11:39:54 +0200

    number and operator functionality

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+const previousScreenNumber = document.querySelector(".previous_number");
+const currentScreenNumber = document.querySelector(".current_number");
+const numberButton = document.querySelectorAll(".number");
+const operatorButton = document.querySelectorAll(".operator");
+const clearButton = document.querySelector("#clear");
+const decimalButton = document.querySelector('.decimal');
+const equalButton = document.querySelector(".equal");
+
+let currentNum = "";
+let operator = "+";
+let previousNum = "";
+
 function addNumbers (num1, num2){
   return num1 + num2;
 }
@@ -13,10 +25,6 @@ function multiplyNumbers(num1, num2){
 function divideNumbers(num1, num2){
   return num1 / num2;
 }
-
-let num1 = 4;
-let operator = "+";
-let num2 = 5;
 
 function operate(operator, num1, num2) {
   if (operator === "+"){
@@ -35,4 +43,27 @@ function operate(operator, num1, num2) {
     console.log("invalid operator");
     return null;
   }
+}
+
+numberButton.forEach(btn => {
+  btn.addEventListener("click", () => handleNumber(btn.textContent));
+});
+
+function handleNumber(number){
+  if(currentNum.length <= 10){
+  currentNum += number;
+  currentScreenNumber.textContent = currentNum;
+  }  
+}
+
+operatorButton.forEach(btn => {
+  btn.addEventListener("click", () => handleOperator(btn.textContent));
+});
+
+function handleOperator(op){
+  operator = op;
+  previousNum = currentNum;
+  previousScreenNumber.textContent = previousNum + operator;
+  currentNum = "";
+  currentScreenNumber = "";
 }
